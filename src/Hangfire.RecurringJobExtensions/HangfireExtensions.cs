2a32f276bdcee3668e30d7c7a45d4f80d7e53f5a
icsharp <554351220@qq.com>
2016-11-16 20:18:51 +0800

    add sln.

---
+++ src/Hangfire.RecurringJobExtensions/HangfireExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hangfire.RecurringJobExtensions
+{
+	/// <summary>
+	/// Hangfire <see cref="RecurringJob"/> extensions.
+	/// </summary>
+	public static class HangfireExtensions
+	{
+		/// <summary>
+		/// Build <see cref="RecurringJob"/> automatically within specified interface or class.
+		/// </summary>
+		/// <param name="configuration"><see cref="IGlobalConfiguration"/></param>
+		/// <param name="types">Specified interface or class</param>
+		/// <returns><see cref="IGlobalConfiguration"/></returns>
+		public static IGlobalConfiguration UseRecurringJob(this IGlobalConfiguration configuration, params Type[] types)
+		{
+			return UseRecurringJob(configuration, () => types);
+		}
+
+		/// <summary>
+		/// Build <see cref="RecurringJob"/> automatically within specified interface or class.
+		/// </summary>
+		/// <param name="configuration"><see cref="IGlobalConfiguration"/></param>
+		/// <param name="typesProvider">The provider to get specified interfaces or class.</param>
+		/// <returns><see cref="IGlobalConfiguration"/></returns>
+		public static IGlobalConfiguration UseRecurringJob(this IGlobalConfiguration configuration, Func<IEnumerable<Type>> typesProvider)
+		{
+			if (typesProvider == null) throw new ArgumentNullException(nameof(typesProvider));
+
+			IRecurringJobBuilder builder = new RecurringJobBuilder(new RecurringJobRegistry());
+
+			builder.Build(typesProvider);
+
+			return configuration;
+		}
+	}
+}
