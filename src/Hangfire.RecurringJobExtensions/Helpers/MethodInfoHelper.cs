9e791c1753fea3450f6de14443fbd29022ac7c36
Kevin <kevin_gp-0052@ie8.com>
2018-09-28 11:53:10 +0800

    init fork version

---
+++ src/Hangfire.RecurringJobExtensions/Helpers/MethodInfoHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace Hangfire.RecurringJobExtensions.Helpers
+{
+    /// <summary>   A method information helper. </summary>
+    public static class MethodInfoHelper
+    {
+        /// <summary>
+        /// Given a lambda expression that calls a method, returns the method info.
+        /// </summary>
+        /// <param name="expression">The expression.</param>
+        /// <returns></returns>
+        public static MethodInfo GetMethodInfo(this Expression<Action> expression)
+        {
+            return GetMethodInfo((LambdaExpression)expression);
+        }
+
+        /// <summary>
+        /// Given a lambda expression that calls a method, returns the method info.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expression">The expression.</param>
+        /// <returns></returns>
+        public static MethodInfo GetMethodInfo<T>(this Expression<Action<T>> expression)
+        {
+            return GetMethodInfo((LambdaExpression)expression);
+        }
+
+        /// <summary>   Given a lambda expression that calls a method, returns the method info. </summary>
+        /// <typeparam name="T">        . </typeparam>
+        /// <typeparam name="TResult">  Type of the result. </typeparam>
+        /// <param name="expression">   The expression. </param>
+        /// <returns>   The method information. </returns>
+        public static MethodInfo GetMethodInfo<T, TResult>(this Expression<Func<T, TResult>> expression)
+        {
+            return GetMethodInfo((LambdaExpression)expression);
+        }
+
+        /// <summary>
+        /// Given a lambda expression that calls a method, returns the method info.
+        /// </summary>
+        /// <param name="expression">The expression.</param>
+        /// <returns></returns>
+        public static MethodInfo GetMethodInfo(this LambdaExpression expression)
+        {
+            MethodCallExpression outermostExpression = expression.Body as MethodCallExpression;
+
+            if (outermostExpression == null)
+            {
+                throw new ArgumentException("Invalid Expression. Expression should consist of a Method call only.");
+            }
+
+            return outermostExpression.Method;
+        }
+    }
+}
